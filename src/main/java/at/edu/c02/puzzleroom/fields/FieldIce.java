d5d2c8c65a0cba669845e5d537153c8e1c279cdb
Simon <s44650@edu.campus02.at>
2025-11-14 15:42:53 +0100

    #1 FieldIce Business Logik wurde implementiert

--- src/main/java/at/edu/c02/puzzleroom/fields/FieldIce.java
+++ src/main/java/at/edu/c02/puzzleroom/fields/FieldIce.java
@@ -2,9 +2,10 @@ package at.edu.c02.puzzleroom.fields;
 
 import at.edu.c02.puzzleroom.Direction;
 import at.edu.c02.puzzleroom.GameBoard;
+import at.edu.c02.puzzleroom.exceptions.PuzzleRoomInvalidArgumentsException;
 
 /**
- * A path field. The player can freely walk onto this field, without any special effects.
+ *
  */
 public class FieldIce extends BaseField {
     public FieldIce(GameBoard gameBoard, char name, int row, int col) {
@@ -15,12 +16,37 @@ public class FieldIce extends BaseField {
     }
 
     public boolean enterField(Direction direction) {
+
         gameBoard.getPlayer().walkStep();
         setPlayerPositionToField();
+
+        Field next = getNextInDirection(direction);
+
+        boolean success = next.enterField(direction);
+
+        if (success) {
+            return true;
+        }
+
         return true;
     }
 
     public boolean leaveField(Direction direction) {
         return true;
+    }
+
+    public Field getNextInDirection(Direction direction) {
+        switch (direction) {
+            case Up:
+                return gameBoard.getField(row - 1, col);
+            case Down:
+                return gameBoard.getField(row + 1, col);
+            case Left:
+                return gameBoard.getField(row, col - 1);
+            case Right:
+                return gameBoard.getField(row, col + 1);
+            default:
+                return null;
+        }
     }
 }
