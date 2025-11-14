20ce9f83257679eb2081a12f4826f9c408cd9f61
Max Tiefengraber <max.tiefengraber@chax.at>
2025-11-14 15:04:14 +0100

    Issue 2

---
+++ src/main/java/at/edu/c02/puzzleroom/fields/FieldOneWay.java
@@ -0,0 +1,33 @@
+package at.edu.c02.puzzleroom.fields;
+
+import at.edu.c02.puzzleroom.Direction;
+import at.edu.c02.puzzleroom.GameBoard;
+
+public class FieldOneWay extends BaseField {
+
+    public FieldOneWay(GameBoard gameBoard, char name, int row, int col) {
+        super(gameBoard, name, row, col);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public boolean enterField(Direction direction) {
+        gameBoard.getPlayer().walkStep();
+        setPlayerPositionToField();
+        return true;
+    }
+
+    @Override
+    public boolean leaveField(Direction direction) {
+        return switch (name) {
+            case '^' -> direction == Direction.Up;
+            case 'v' -> direction == Direction.Down;
+            case '<' -> direction == Direction.Left;
+            case '>' -> direction == Direction.Right;
+            default -> false;
+        };
+    }
+}
